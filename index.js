631a25267e2cb4e81d30cd3ef8f2e9b75b1fff50
Vu <vu.nguyen101@gmail.com>
2022-02-13 15:01:40 +0200

    write logs into a text file

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const fs = require('fs')
+
 const getRandomString = () => {
   const randomString = Math.random().toString(36).substring(2, 10)
   let timestamp = new Date()
 
-  console.log(`${timestamp} : - ${randomString}`)
+  fs.appendFile('./log.txt', `${timestamp} : - ${randomString}\n`, err => {
+    if (err) throw err
+  })
 
+  console.log(`${timestamp} : - ${randomString}`)
   setTimeout(getRandomString, 5000)
 }
 
